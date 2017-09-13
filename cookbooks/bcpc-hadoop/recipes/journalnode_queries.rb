39b19d4aac3422f63f749d13ac2a95a95cab5b99
Lei Chen <lchen403@bloomberg.net>
2017-09-13 11:23:20 -0400

    added alarms for journalnode txid and epoch

--- cookbooks/bcpc-hadoop/recipes/journalnode_queries.rb
+++ cookbooks/bcpc-hadoop/recipes/journalnode_queries.rb
@@ -1,13 +1,23 @@
-triggers_sensitivity = "60m"
+triggers_sensitivity = '60m'
 
 node.set[:bcpc][:hadoop][:graphite][:service_queries][:journalnode] = {
   'journalnode.LastWrittenTxId' => {
      'query' => "rangeOfSeries(jmx.journalnode.dnj2-bach-*.journal_node.Journal-DNJ2-JR.LastWrittenTxId)",
-     'trigger_val' => "min(#{trigger_sensitivity})",
+     'trigger_val' => "min(#{triggers_sensitivity})",
      'trigger_cond' => ">0",
      'trigger_name' => "JournalNodeLastWrittenTxId",
      'enable' => true,
-     'trigger_desc' => "JournalNodes have different LastWrittenTxId for #{trigger_sensitivity}",
+     'trigger_desc' => "Journal nodes have different LastWrittenTxId for #{triggers_sensitivity}",
+     'severity' => 3,
+     'route_to' => "admin"
+  },
+  'journalnode.LastPromisedEpoch' => {
+     'query' => "rangeOfSeries(jmx.journalnode.dnj2-bach-*.journal_node.Journal-*.LastPromisedEpoch)",
+     'trigger_val' => "min(#{triggers_sensitivity})",
+     'trigger_cond' => ">0",
+     'trigger_name' => "JournalNodeLastPromisedEpoch",
+     'enable' => true,
+     'trigger_desc' => "Journal nodes have different LastPromisedEpoch for #{triggers_sensitivity}",
      'severity' => 3,
      'route_to' => "admin"
   }
